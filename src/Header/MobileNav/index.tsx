1bf78c095fc1f4e5164f17ce1406da29caea2487
Ronny Perez <ronnypdev@gmail.com>
2025-02-26 10:10:15 -0500

    Overlay WIP

--- src/Header/MobileNav/index.tsx
+++ src/Header/MobileNav/index.tsx
@@ -9,39 +9,43 @@ type MobileNav = {
 
 export default function MobileNav({ logo, mobileListItems }: MobileNav) {
   return (
-    <div className="max-w-full flex items-center justify-between py-[30px] md:hidden max-mxl:pl-8 max-mxl:pr-8">
-      <picture>
-        <source srcSet="/abstractly.svg" />
-        <Image src="/abstractly.svg" alt="abstractly logo" width={112} height={32} />
-      </picture>
-      <nav className="nav-list-wrapper ml-10 grow hidden mxl:block">
-        <ul className="flex items-start">
-          <li className="ml-12">Home</li>
-          <li className="ml-12">Features</li>
-          <li className="ml-12">Pricing</li>
-          <li className="ml-12">About us</li>
-          <li className="ml-12">Contact</li>
-        </ul>
-      </nav>
-      <div className="btn-group hidden mxl:block">
-        <Button
-          text="Learn More"
-          textColor="text-black"
-          bgColor="bg-white"
-          hover="bg-pale-white"
-          focus="bg-pale-white"
-          disabledButton={false}
-        />
-        <Button
-          text="See Pricing"
-          textColor="text-white"
-          bgColor="bg-indigo-700"
-          hover="bg-indigo-800"
-          focus="bg-indigo-800"
-          disabledButton={false}
-        />
+    <div className="fixed top-0 bottom-0 w-(--mobile-nav-width) bg-white mxl:hidden">
+      <div className="max-w-full flex flex-col items-center h-full min-h-full py-[30px]">
+        <div className="mobile-logo-wrapper w-full flex justify-between items-center">
+          <picture>
+            <source srcSet="/abstractly.svg" />
+            <Image src="/abstractly.svg" alt="abstractly logo" width={112} height={32} />
+          </picture>
+          <X className="hidden max-mxl:block cursor-pointer" />
+        </div>
+        <nav className="nav-list-wrapper w-full min-h-auto">
+          <ul className="flex flex-col items-baseline">
+            <li className="mb-3">Home</li>
+            <li className="mb-3">Features</li>
+            <li className="mb-3">Pricing</li>
+            <li className="mb-3">About us</li>
+            <li className="mb-3">Contact</li>
+          </ul>
+        </nav>
+        <div className="btn-group">
+          <Button
+            text="Learn More"
+            textColor="text-black"
+            bgColor="bg-white"
+            hover="bg-pale-white"
+            focus="bg-pale-white"
+            disabledButton={false}
+          />
+          <Button
+            text="See Pricing"
+            textColor="text-white"
+            bgColor="bg-indigo-700"
+            hover="bg-indigo-800"
+            focus="bg-indigo-800"
+            disabledButton={false}
+          />
+        </div>
       </div>
-      <X className="hidden max-mxl:block cursor-pointer" />
     </div>
   )
 }
--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -1,3 +1,4 @@
+import MobileNav from '../MobileNav'
 import Image from 'next/image'
 import Button from '@/components/Button'
 import { Menu } from 'lucide-react'
@@ -9,39 +10,42 @@ type NavBar = {
 
 export default function NavBar({ logo, listItems }: NavBar) {
   return (
-    <div className="max-w-full flex items-center justify-between py-[30px] max-ml:hidden max-mxl:pl-8 max-mxl:pr-8">
-      <picture>
-        <source srcSet="/abstractly.svg" />
-        <Image src="/abstractly.svg" alt="abstractly logo" width={112} height={32} />
-      </picture>
-      <nav className="nav-list-wrapper ml-10 grow hidden mxl:block">
-        <ul className="flex items-start">
-          <li className="ml-12">Home</li>
-          <li className="ml-12">Features</li>
-          <li className="ml-12">Pricing</li>
-          <li className="ml-12">About us</li>
-          <li className="ml-12">Contact</li>
-        </ul>
-      </nav>
-      <div className="btn-group hidden mxl:block">
-        <Button
-          text="Learn More"
-          textColor="text-black"
-          bgColor="bg-white"
-          hover="bg-pale-white"
-          focus="bg-pale-white"
-          disabledButton={false}
-        />
-        <Button
-          text="See Pricing"
-          textColor="text-white"
-          bgColor="bg-indigo-700"
-          hover="bg-indigo-800"
-          focus="bg-indigo-800"
-          disabledButton={false}
-        />
+    <>
+      <div className="max-w-full flex items-center justify-between py-[30px] max-mxl:hidden max-mxl:pl-7 max-mxl:pr-7">
+        <picture>
+          <source srcSet="/abstractly.svg" />
+          <Image src="/abstractly.svg" alt="abstractly logo" width={112} height={32} />
+        </picture>
+        <nav className="nav-list-wrapper ml-10 grow hidden mxl:block">
+          <ul className="flex items-start">
+            <li className="ml-12">Home</li>
+            <li className="ml-12">Features</li>
+            <li className="ml-12">Pricing</li>
+            <li className="ml-12">About us</li>
+            <li className="ml-12">Contact</li>
+          </ul>
+        </nav>
+        <div className="btn-group hidden mxl:block">
+          <Button
+            text="Learn More"
+            textColor="text-black"
+            bgColor="bg-white"
+            hover="bg-pale-white"
+            focus="bg-pale-white"
+            disabledButton={false}
+          />
+          <Button
+            text="See Pricing"
+            textColor="text-white"
+            bgColor="bg-indigo-700"
+            hover="bg-indigo-800"
+            focus="bg-indigo-800"
+            disabledButton={false}
+          />
+        </div>
+        <Menu className="hidden max-mxl:block cursor-pointer" />
       </div>
-      <Menu className="hidden max-mxl:block cursor-pointer" />
-    </div>
+      <MobileNav />
+    </>
   )
 }
